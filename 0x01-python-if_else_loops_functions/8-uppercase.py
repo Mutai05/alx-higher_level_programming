6fa588c64e1b023035ac78c64933ea1928844d38
Mutai Kelvin <Mutai05>
2023-10-31 14:54:48 -0700

    Task 8 - Write a function that prints a string in uppercase followed by a new line.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    upper_s = ""
+    for char in s:
+        if 97 <= ord(char) <= 122:
+            upper_s += chr(ord(char) - 32)
+        else:
+            upper_s += char
+    print("{:s}".format(upper_s))
+
+# Test the function
+uppercase("best")
+uppercase("Best School 98 Battery street")
