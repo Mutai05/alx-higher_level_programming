87ec1931427ecea0c6e5054adbdf8fb9e98d1203
Mutai Kelvin <Mutai05>
2023-10-31 14:03:54 -0700

    Updated 2-print_alphabet.py file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-# ASCII value of 'a' and 'z'
-ascii_a = ord('a')
-ascii_z = ord('z')
-
-# Loop to print the lowercase alphabet
-for char_ascii in range(ascii_a, ascii_z + 1):
-    print("{:c}".format(char_ascii), end="")
-
-# Print a newline character at the end
-print()
+for a in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(a), end='')
