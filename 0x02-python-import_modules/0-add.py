cebb5320e6b5e3b8c92199eae71b1559b2885a07
Mutai Kelvin <Mutai05>
2023-11-02 14:17:17 -0700

    Updated Task 0 file code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-a = 1
-b = 2
 if __name__ == "__main__":
+    # Import the add function from add_0.py
     from add_0 import add
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+
+    # Assign values to variables a and b
+    a = 1
+    b = 2
+
+    # Calculate the result
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
