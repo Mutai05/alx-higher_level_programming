9dda1ca102ba8a8d5b5f6a2367aeec1cd623a7d3
Mutai Kelvin <Mutai05>
2023-11-02 14:07:38 -0700

    Updated Task 8 file code

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
+    # Import the add and sub functions from magic_calculation_102 module
     from magic_calculation_102 import add, sub
 
     if a < b:
+        # If a is less than b, perform addition
         c = add(a, b)
         for i in range(4, 6):
+            # Iterate and add numbers from 4 to 5
             c = add(c, i)
-        return (c)
-
+        return c
     else:
-        return (sub(a, b))
+        # If a is not less than b, perform subtraction
+        return sub(a, b)
