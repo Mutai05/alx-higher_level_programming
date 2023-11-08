eb256088d6949d65ceb2ff2f5aa0a24e65526593
Mutai Kelvin <Mutai05>
2023-11-08 11:59:48 -0800

    Task 9 - Write a function that returns a new dictionary with all values multiplied by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    """
+    Create a new dictionary with values multiplied by 2.
+
+    Args:
+        a_dictionary (dict): The input dictionary with integer values.
+
+    Returns:
+        dict: A new dictionary with values multiplied by 2.
+    """
+    return {key: value * 2 for key, value in a_dictionary.items()}
