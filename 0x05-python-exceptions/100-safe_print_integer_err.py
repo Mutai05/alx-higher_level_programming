dfc0f87d679064e22a9ab0a5ca10e0c0e80445d8
Mutai05 <kelvok2014@gmail.com>
2023-11-20 14:02:50 -0800

    Task 7 - Write a function that prints an integer.

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        # Try converting the value to an integer and printing it
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        # If any exception occurs, print the error to stderr
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
+
+# Test cases
+if __name__ == "__main__":
+    value = 89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer_err(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
