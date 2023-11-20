331e8037639f7c214c8aa5f48a5d7a7a03ea3536
Mutai05 <kelvok2014@gmail.com>
2023-11-20 13:48:41 -0800

    Task 6 - Fixed pycodestyle formatting code

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -4,9 +4,9 @@ def raise_exception_msg(message=""):
     # Raise a NameError with the provided message
     raise NameError(message)
 
+
 if __name__ == "__main__":
     try:
         raise_exception_msg("C is fun")
     except NameError as ne:
         print(ne)
-
