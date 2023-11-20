2ce21bb83c2df848d1a572cf3b7b8eedeb543b0a
Mutai05 <kelvok2014@gmail.com>
2023-11-20 13:43:19 -0800

    Task 5 - Write a function that raises a type exception.

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    # Attempting an operation that raises a TypeError
+    try:
+        # Trying to add an integer and a string which raises a TypeError
+        result = 1 + "hello"
+    except TypeError:
+        # Raise the TypeError explicitly
+        raise TypeError("Exception raised")
+
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as te:
+        print(te)
