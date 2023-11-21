076ca1409e6c7acbd9d3a4ffb458f4b5fbcdc04f
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-11-22 02:16:23 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,9 @@
 """A class Square that defines a square """
 
 class Square:
+    """Represents a square"""
     def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
         self.__size = size
