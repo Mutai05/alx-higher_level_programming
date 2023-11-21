6e02240b0e2a8d2581b5f5112ca7b561557f4174
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-11-22 02:50:13 +0300

    Updated 103-magic_class.py documentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,13 +6,19 @@ class MagicClass:
     """set up magic class"""
     
     def __init__(self, radius):
+        """ write docstring """
+
         if not isinstance(radius, (int, float)):
             raise TypeError('radius must be a number')
 
         self._MagicClass__radius = radius
 
     def area(self):
+        """again with docstring"""
+        
         return self._MagicClass__radius**2 * math.pi
 
     def circumference(self):
+        """another docstring"""
+        
         return 2 * math.pi * self._MagicClass__radius
