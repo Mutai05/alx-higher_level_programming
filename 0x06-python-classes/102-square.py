1bf0e481d0835a0d1001a6d033e22a6e82c4e44e
Mutai05 <kelvok2014@gmail.com>
2023-11-21 14:30:50 -0800

    Task 9 - Write a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, (int, float)):
+            raise TypeError("size must be a number")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __eq__(self, other):
+        if isinstance(other, Square):
+            return self.area() == other.area()
+
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, Square):
+            return self.area() < other.area()
+
+        raise TypeError("Cannot compare Square with non-Square object")
+
+    def __le__(self, other):
+        return self.__lt__(other) or self == other
+
+    def __gt__(self, other):
+        if isinstance(other, Square):
+            return self.area() > other.area()
+
+        raise TypeError("Cannot compare Square with non-Square object")
+
+    def __ge__(self, other):
+        return self.__gt__(other) or self == other
