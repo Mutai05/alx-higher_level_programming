9ad49f3c447e13d449aca179e1e63cfbd701120e
Mutai05 <kelvok2014@gmail.com>
2023-11-21 14:33:13 -0800

    Task 10 - Write the Python class MagicClass

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class MagicClass:
+    def __init__(self, radius):
+        if not isinstance(radius, (int, float)):
+            raise TypeError('radius must be a number')
+
+        self._MagicClass__radius = radius
+
+    def area(self):
+        return self._MagicClass__radius**2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self._MagicClass__radius
