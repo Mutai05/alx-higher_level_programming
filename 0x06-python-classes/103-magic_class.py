f3bd33ddb73aecae93cc7e152aba46be69456fc2
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-11-22 02:48:09 +0300

    Updated 103-magic_class.py documentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+# 103-magic_class.py by Kelvin KIpkemboi
+"""class MagicClass """
 
 class MagicClass:
+    """set up magic class"""
+    
     def __init__(self, radius):
         if not isinstance(radius, (int, float)):
             raise TypeError('radius must be a number')
