a6b30b4e021fec462a0a3a7cfdbe62d843773ae3
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-11-22 02:24:21 +0300

    Updated 3-square.py documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+# 3-square.py by Kelvin KIpkemboi
+"""Defines a square """
 
 class Square:
+    """A class that represents a square"""
+
     def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if the size is not an integer
+            ValueError: if size is less than zero
+        """
         self.__size = size
 
         if not isinstance(size, int):
@@ -10,4 +21,8 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """
+        Calculate the area of the square
+        Returns: The square of the size
+        """
         return self.__size ** 2
