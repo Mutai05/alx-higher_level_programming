b4f77ecebd366378567e6ee38f957d379c7e8e45
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-11-22 02:21:34 +0300

    Updated 2-square.py documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+# 2-square.py by Kelvin KIpkemboi
+"""Defines a square """
 
 class Square:
+    """A class that represents a square"""
+
     def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if the size is not an integer
+            ValueError: if size is less than zero
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
