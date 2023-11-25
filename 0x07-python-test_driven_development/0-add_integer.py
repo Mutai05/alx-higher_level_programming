50aaa030fb1b33d78d1ef499612581cf257de277
Mutai05 <kelvok2014@gmail.com>
2023-11-25 13:46:16 -0800

    Updated task 0 code files - Write a function that adds 2 integers.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,49 @@
 #!/usr/bin/python3
+
 def add_integer(a, b=98):
     """
     Adds two integers.
 
     Args:
-    a (int or float): First number.
-    b (int or float): Second number (default is 98).
+    a (int): The first integer.
+    b (int): The second integer. Defaults to 98.
 
     Returns:
-    int: Sum of a and b.
+    int: The sum of a and b.
 
     Raises:
-    TypeError: If a or b is not an integer or float.
+    TypeError: If a or b is not an integer or a float.
+
+    Examples:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(-1, 2)
+    1
+    >>> add_integer(1.5, 2.9)
+    3
+    >>> add_integer(-2.9, 1)
+    -1
+    >>> add_integer(1)
+    99
+    >>> add_integer(None)
+    Traceback (most recent call last):
+    ...
+    TypeError: b must be an integer
+    >>> add_integer(float("inf"))
+    Traceback (most recent call last):
+    ...
+    TypeError: add_integer() takes from 1 to 2 positional arguments but 3 were given
+    >>> add_integer("hey")
+    Traceback (most recent call last):
+    ...
+    TypeError: a must be an integer
+    >>> add_integer(0, [1, 2, 3])
+    Traceback (most recent call last):
+    ...
+    TypeError: b must be an integer
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
     return int(a) + int(b)
