a8909328eecb119a5f3cf37f3b4ac520ba2d3407
Mutai05 <kelvok2014@gmail.com>
2024-01-16 14:59:01 -0800

    A script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,9 +1,6 @@
 -- 16-no_link.sql
 -- Task: List all records of the table second_table with a valid name, ordered by descending score
-
 -- Use the specified database
-USE hbtn_0c_0;
-
 -- List all records with a valid name, ordered by descending score
 
 SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
