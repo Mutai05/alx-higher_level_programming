3b70904b68085ad66cc80cd5971566230b8ce73b
Mutai05 <kelvok2014@gmail.com>
2024-01-16 14:01:43 -0800

    A script that deletes the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- 2-remove_database.sql
+-- Task: Delete the database hbtn_0c_0 if it exists
+
+-- Try dropping the database. If it doesn't exist, ignore the error.
+DROP DATABASE IF EXISTS hbtn_0c_0;
