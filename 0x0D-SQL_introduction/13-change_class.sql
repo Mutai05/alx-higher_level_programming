1e8ecb2e2bfc13a57b2d59c1ad00a3ae90f3ad2c
Mutai05 <kelvok2014@gmail.com>
2024-01-16 15:09:09 -0800

    Updated code files

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,8 +1,6 @@
 -- 13-change_class.sql
 -- Task: Remove all records with a score <= 5 in the table second_table
-
 -- Use the specified database
-USE hbtn_0c_0;
-
 -- Delete records with score <= 5 from second_table
+
 DELETE FROM second_table WHERE score <= 5;
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,8 +1,6 @@
 -- 14-average.sql
 -- Task: Compute the average score of all records in the table second_table
-
 -- Use the specified database
-USE hbtn_0c_0;
-
 -- Compute the average score from second_table
-SELECT AVG(score) AS 'average' FROM second_table;
+
+SELECT AVG(score) AS average FROM second_table;
