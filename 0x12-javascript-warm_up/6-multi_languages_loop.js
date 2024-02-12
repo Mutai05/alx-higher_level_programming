06b226437a645398df35222842326fc1ae2edd24
Mutai05 <kelvok2014@gmail.com>
2024-02-12 12:40:20 -0800

    Updated Task 6 code file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (const line of lines) {
-    console.log(line);
+for (const element of myVar) {
+    console.log(element);
 }
