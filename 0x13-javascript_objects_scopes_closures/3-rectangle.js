83d3a09676e3c2f49567434d490c1c23565ff3d0
Mutai05 <kelvok2014@gmail.com>
2024-02-13 11:32:48 -0800

    Task 3 - Write a class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+
+  print () {
+    // Print the rectangle using the character 'X'
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
