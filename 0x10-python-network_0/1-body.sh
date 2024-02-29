4f739857c54e14cbfade9cde5d2bad3315d0e7d9
Mutai05 <kelvok2014@gmail.com>
2024-02-29 13:28:37 -0800

    Updated code file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request, and displays the body of the response (only for 200 status code)
-
-# Send a GET request to the specified URL, discard progress info (-s), and display the body for a 200 status code
-curl -s -o /dev/null -w "%{http_code}" "$1" | {
-    read -r status_code
-    if [ "$status_code" -eq 200 ]; then
-        curl -s "$1"
-    fi
-}
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
