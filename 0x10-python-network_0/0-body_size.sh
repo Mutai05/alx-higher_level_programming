91041d5eb4b24b0858ef4f2ef9a464d4b20d657d
Mutai05 <kelvok2014@gmail.com>
2024-02-29 13:16:09 -0800

    Updated code file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request, and displays the size of the body in bytes
-
-# Send a GET request to the specified URL, discard progress info (-s), and display the size of the body
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}' 
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
