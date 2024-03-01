fcf045f85d1b7853e4b07b1fb75de33602223840
Mutai05 <kelvok2014@gmail.com>
2024-03-01 14:52:42 -0800

    Task 10 - updated code file

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,29 +7,15 @@ import requests
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: {} <repository name> <repository owner>".format(sys.argv[0]))
-        sys.exit(1)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
-
+    r = requests.get(url)
+    commits = r.json()
     try:
-        r = requests.get(url)
-
-        # Check if the request was successful (status code 200)
-        r.raise_for_status()
-
-        commits = r.json()
-
-        for i in range(min(10, len(commits))):
-            commit = commits[i]
-            sha = commit.get("sha")
-            author_name = commit.get("commit", {}).get("author", {}).get("name", "Unknown")
-            print("{}: {}".format(sha, author_name))
-
-    except requests.RequestException as e:
-        print("Error: {}".format(e))
-    except ValueError:
-        print("Error: Unable to parse JSON response.")
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
-        print("Error: Insufficient number of commits.")
+        pass
