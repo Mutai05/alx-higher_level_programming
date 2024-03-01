4f6592938d05697298d49ef1e81e36a6aa74c0b0
Mutai05 <kelvok2014@gmail.com>
2024-03-01 15:07:48 -0800

    Task 5 - fixed code format style

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request, and displays the value of the variable X-Request-Id in the response header"""
+"""Takes in a URL, sends a request, and displays the value of
+the variable X-Request-Id in the response header"""
 import requests
 import sys
 
