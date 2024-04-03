0755ecea78da87b4a0b4ce268cc45bcf0a58a484
Mutai05 <kelvok2014@gmail.com>
2024-04-03 14:13:44 -0700

    Task 13 - a JavaScript script that fetches and prints how to say “Hello” depending on the language

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#btn_translate').click(fetchTranslation);
+  $('#language_code').keypress(function(event) {
+    if (event.keyCode === 13) {
+      fetchTranslation();
+    }
+  });
+
+  function fetchTranslation() {
+    const languageCode = $('#language_code').val();
+    const apiUrl = 'https://www.fourtonfish.com/hellosalut/hello/';
+
+    $.get(apiUrl, { lang: languageCode }, function(data) {
+      $('#hello').text(data.hello);
+    });
+  }
+});
